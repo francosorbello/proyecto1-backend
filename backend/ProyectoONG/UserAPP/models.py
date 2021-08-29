b692668f998bcaee7b108c8c14266b8193387ea2
JuanmaRomero <jmromero1899@gmail.com>
2021-08-29 20:19:56 -0300

    unos arreglos en el modelo user

--- backend/ProyectoONG/UserAPP/models.py
+++ backend/ProyectoONG/UserAPP/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    rol = models.IntegerField(max_length=4)
+    rol = models.IntegerField()
     name = models.CharField(max_length=25)
     mail = models.EmailField(max_length=50)
     password = models.CharField(max_length=15)
