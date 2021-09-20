d7832cfc7b9750a96690626150477b8f13b1cefe
francosorbello <franco.sorbelo@gmail.com>
2021-09-20 18:19:14 -0300

    added patch operation to donation view

--- backend/ProyectoONG/DonationAPP/views.py
+++ backend/ProyectoONG/DonationAPP/views.py
@@ -30,13 +30,13 @@ class DonationAPPView(APIView):
     def post(self,request):
         """Recibe datos dentro del request para guardar un nuevo Donation en la base de datos"""
         serializer = self.serializer_class(data=request.data)
-        
         if(serializer.is_valid()):
             newDonation = serializer.create(serializer.validated_data)
             newDonation.save()
             msg = "Donation created succesfully"
-            return Response({'message':msg})
+            return Response({'message':msg,'id':newDonation.id})
         else:
+            print(serializer.errors)
             return Response(serializer.errors)
 
     def put(self,request,pk=None):
@@ -49,12 +49,18 @@ class DonationAPPView(APIView):
         else:
             return Response(serializedDonation.errors)
 
-    # def patch(self,request,pk=None):
-    #     '''Actualiza valores de un objeto en vez de por completo'''
-    #     user = User.objects.get(id=pk)
-        
-    #     msg = "PATCH en objeto con nombre "+user.nombre
-    #     return Response({"message":msg})
+    def patch(self,request,pk=None):
+        '''Actualiza valores de un objeto en vez de por completo'''
+        print("patching user with id: ")
+        print(pk)
+        donation = Donation.objects.get(id=pk)
+        serializedDonation = DonationSerializer(donation,data=request.data,partial=True)
+        if(serializedDonation.is_valid()):
+            serializedDonation.save()
+            msg = "PATCHed succesfuly"
+            return Response({"message":msg})
+        else:
+            return JsonResponse(code=400, data="wrong parameters")
 
     def delete(self,request,pk=None):
 
